8ea937a38db1c207451446b668f1015e26f7393b
Adrea Araoz <adreaaraoz@Adreas-MacBook-Pro.local>
2019-07-16 14:38:38 -0400

    adding js functions

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
   $('#go-btn').click(function(){
-    $('leadModal').toggle();
+    var zipcode = $('#zipcode').val ();
+    $('#smallZip').val (zipcode);
   });
+
+
+$('#quote_submit').click(function(e) {
+    console.log("submitted");
+
+  });
+
+  });
+  $("#quote_submit").click(function(e){
+    e.preventDefault();
+
+    formSubmit('smallFrom');
+  });
+
 });
+
+  function formSubmit(name) {
+  var serializedData = $(document.forms[name]) .serializeObject( );
+  var url = 'https://script.google.com/macros/s/AKfycbwRvZnrx0RrDaHgRp-bJ-Q5uECNwIKTx9GTR4xaBXSE2ZvDWpc/exec'; // get this url
+
+  var jqxhr = $.ajax({
+    url,
+    method: "GET",
+    dataType: "json",
+    data: serializedData,
+    success: function(data){
+      console.log('success', data);
+    }
+  });
+}
